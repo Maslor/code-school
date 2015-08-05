3208a8e1ffad87d5e970431aaf6829b27b60fc39
Patcholito <Patcholito@users.noreply.github.com>
2015-08-05 22:19:22 +0100

    Update lyear.py

--- challenge3/lyear.py
+++ challenge3/lyear.py
@@ -1,1 +1,19 @@
 
+while True:
+	year = eval(input("Insert a year: "))
+	
+	if ((year % 4 ) == 0):
+		print ("Noob Year")
+		answer = input("Another year? (yes or no?) ")
+		if answer == "no":
+			break		
+	else:
+		print ("Not leap")
+	
+	
+	
+		answer = input("Another year? (yes or no?) ")
+		if answer == "no":
+			break
+	   
+print( "Bye! :D")
